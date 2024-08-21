70b3d964e9551e1fd9e02bcec04910713b592946
felipeam10 <felipeam10@hotmail.com>
2024-08-21 01:43:04 -0300

    vJSR-10.07 - hello world refatoring

--- src/main/java/com/felipeam10/helloworldspringbatch/step/PrintHelloStepConfig.java
+++ src/main/java/com/felipeam10/helloworldspringbatch/step/PrintHelloStepConfig.java
@@ -1,27 +1,23 @@
-package com.felipeam10.helloworldspringbatch.config;
+package com.felipeam10.helloworldspringbatch.step;
 
-import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-public class BatchConfig {
+public class PrintHelloStepConfig {
 
     @Bean
-    public Job job(JobRepository jobRepository, Step printHelloStep) {
-        return new JobBuilder("job", jobRepository)
-                .start(printHelloStep)
-                .incrementer(new RunIdIncrementer())
+    public Step printHelloStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager, Tasklet printHelloTasklet) {
+        return new StepBuilder("printHelloStep", jobRepository)
+                .tasklet(printHelloTasklet, platformTransactionManager)
                 .build();
     }
-
 }
