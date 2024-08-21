d565fa64f9180de716664d7abb06c2da76819c25
felipeam10 <felipeam10@hotmail.com>
2024-08-21 02:19:22 -0300

    vJSR-10.08 - hello world refatoring using environment variables

--- src/main/java/com/felipeam10/helloworldspringbatch/tasklet/PrintHelloTasklet.java
+++ src/main/java/com/felipeam10/helloworldspringbatch/tasklet/PrintHelloTasklet.java
@@ -5,15 +5,19 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @StepScope
 public class PrintHelloTasklet implements Tasklet {
 
+    @Value("${name}")
+    private String name;
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-        System.out.println("Hello World Spring Batch!");
+        System.out.println("Hello " + name + " World Spring Batch!");
         return RepeatStatus.FINISHED;
     }
 }
